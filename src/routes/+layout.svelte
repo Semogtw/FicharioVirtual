b7736f371cba2969e5def131e96b82000d6b1830
Arthur Almeida <arthurgva1602@gmail.com>
2026-08-02 02:12:02 -0300

    feat: wire the global editorial application shell

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import AppShell from '$lib/components/AppShell.svelte';
+	import '$lib/design/global.css';
+
+	let { children } = $props();
+</script>
+
+<AppShell>
+	{@render children()}
+</AppShell>
