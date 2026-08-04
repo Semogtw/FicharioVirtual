715b0878eac2f147413dcdfb2b68779523a38bdc
Arthur Almeida <arthurgva1602@gmail.com>
2026-08-04 14:54:55 -0300

    feat: resume OCR queue with authorized sessions

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,8 +1,17 @@
 import { invalidateAll } from '$app/navigation';
 import type { ClientInit } from '@sveltejs/kit';
-import { initializeSession, startSessionTracking } from '$lib/stores/session.svelte';
+import { pauseQueue, resumeQueue } from '$lib/import/job-runner';
+import {
+	initializeSession,
+	startSessionTracking,
+	subscribeSessionAuthorization
+} from '$lib/stores/session.svelte';
 
 export const init: ClientInit = () => {
+	subscribeSessionAuthorization((authorized) => {
+		if (authorized) void resumeQueue();
+		else pauseQueue();
+	});
 	void initializeSession();
 	try {
 		startSessionTracking(() => void invalidateAll());
